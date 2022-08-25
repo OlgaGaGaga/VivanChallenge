09d023db260685a26039b9ed3160d0cfaef26ffe
OlgaGaGaga <okuzmich2005@gmail.com>
2022-08-25 16:39:03 +0300

    remove unnecessary functions

--- main.py
+++ main.py
@@ -11,22 +11,11 @@ def connect_db(in_host, in_user, in_passwd, in_db):
     return mydb
 
 
-def init_cursor(mydb):
-    mycursor = mydb.cursor()
-
-    return mycursor
-
-def cursor_execute(cursor, name):
-
-    cursor.execute("CREATE DATABASE " + name)
-
-
-
 
 if __name__ == '__main__':
 
     mydb = connect_db('localhost', 'root', 'kuznechik', 'testdb')
-    mycursor = init_cursor(mydb=mydb)
+    mycursor = mydb.cursor()
 
     # create_db(mycursor, 'testdb')
 
