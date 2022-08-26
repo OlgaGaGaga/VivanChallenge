8a33f1ed215586bf94719cdcc04ec244f61b4701
OlgaGaGaga <okuzmich2005@gmail.com>
2022-08-26 14:47:07 +0300

    json processing function added to main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import pandas as pd
-
+import json
 def connect_db(in_host, in_user, in_passwd, in_db):
     mydb = mysql.connector.connect(
         host=in_host,
@@ -12,11 +12,84 @@ def connect_db(in_host, in_user, in_passwd, in_db):
     return mydb
 
 
-def read_json(file):
-    the_file = pd.read_json(file)
+def read_process_json(file):
+    with open(file, 'r') as f:
+        json_data = json.load(f)
 
-    return the_file
+    table_dict = {'patID': [], 'upHsgene': [], 'upDmgene': [], 'upDELDUPL': [], 'upConfidence': [], 'upComments': [],
+                  'downHsgene': [], 'downDmgene': [], 'downDELDUPL': [], 'downConfidence': [], 'downComments': []}
 
+    for n in range(len(json_data['entries'])):
+
+        patID = json_data['entries'][n]['name'][:6]
+
+        upHsgene = ''
+        upDmgene = ''
+        upDELDUPL = ''
+        upConfidence = ''
+        upComments = ''
+        downHsgene = ''
+        downDmgene = ''
+        downDELDUPL = ''
+        downConfidence = ''
+        downComments = ''
+
+        for i in range(len(json_data['entries'][n]['days'][0]['notes'])):
+            if 'table' in json_data['entries'][n]['days'][0]['notes'][i].keys():
+
+                if json_data['entries'][n]['days'][0]['notes'][i]['table']['name'] == 'Genes to up regulate':
+
+                    for j in range(len(json_data['entries'][n]['days'][0]['notes'][i]['table']['rows'])):
+                        upHsgene = upHsgene + \
+                                   json_data['entries'][n]['days'][0]['notes'][i]['table']['rows'][j]['cells'][0][
+                                       'text'].replace('Hs-', '') + ','
+                        upDmgene = upDmgene + \
+                                   json_data['entries'][n]['days'][0]['notes'][i]['table']['rows'][j]['cells'][1][
+                                       'text'].replace('Dm-', '') + ','
+                        upDELDUPL = upDELDUPL + \
+                                    json_data['entries'][n]['days'][0]['notes'][i]['table']['rows'][j]['cells'][2][
+                                        'text'] + ','
+                        upConfidence = upConfidence + \
+                                       json_data['entries'][n]['days'][0]['notes'][i]['table']['rows'][j]['cells'][3][
+                                           'text'] + ','
+                        upComments = upComments + \
+                                     json_data['entries'][n]['days'][0]['notes'][i]['table']['rows'][j]['cells'][4][
+                                         'text'] + ','
+
+                if json_data['entries'][n]['days'][0]['notes'][i]['table']['name'] == 'Genes to down regulate':
+
+                    for j in range(len(json_data['entries'][n]['days'][0]['notes'][i]['table']['rows'])):
+                        downHsgene = downHsgene + \
+                                     json_data['entries'][n]['days'][0]['notes'][i]['table']['rows'][j]['cells'][0][
+                                         'text'].replace('Hs-', '') + ','
+                        downDmgene = downDmgene + \
+                                     json_data['entries'][n]['days'][0]['notes'][i]['table']['rows'][j]['cells'][1][
+                                         'text'].replace('Dm-', '') + ','
+                        downDELDUPL = downDELDUPL + \
+                                      json_data['entries'][n]['days'][0]['notes'][i]['table']['rows'][j]['cells'][2][
+                                          'text'] + ','
+                        downConfidence = downConfidence + \
+                                         json_data['entries'][n]['days'][0]['notes'][i]['table']['rows'][j]['cells'][3][
+                                             'text'] + ','
+                        downComments = downComments + \
+                                       json_data['entries'][n]['days'][0]['notes'][i]['table']['rows'][j]['cells'][4][
+                                           'text'] + ','
+
+
+        table_dict['patID'].append(patID)
+        table_dict['upHsgene'].append(upHsgene[:-1])
+        table_dict['upDmgene'].append(upDmgene[:-1])
+        table_dict['upDELDUPL'].append(upDELDUPL[:-1])
+        table_dict['upConfidence'].append(upConfidence[:-1])
+        table_dict['upComments'].append(upComments[:-1])
+        table_dict['downHsgene'].append(downHsgene[:-1])
+        table_dict['downDmgene'].append(downDmgene[:-1])
+        table_dict['downDELDUPL'].append(downDELDUPL[:-1])
+        table_dict['downConfidence'].append(downConfidence[:-1])
+        table_dict['downComments'].append(downComments[:-1])
+
+    table = pd.DataFrame.from_dict(table_dict)
+    return table
 
 if __name__ == '__main__':
 
@@ -49,7 +122,7 @@ if __name__ == '__main__':
     # mydb.commit()
 
 
-    json_file = read_json('../Downloads/Vivan/input_files/benchling_entries.json')
+    table = read_process_json('../Downloads/Vivan/input_files/benchling_entries.json')
 
-    print(json_file.head())
+    print(table)
 
