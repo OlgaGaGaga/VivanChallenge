27fd122a54790f7844045e29f090230fc19e316a
OlgaGaGaga <okuzmich2005@gmail.com>
2022-08-28 22:38:00 +0300

    SQL code updated

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -70,7 +70,7 @@ FROM (
 	GROUP BY Patient_ID) as S
 WHERE Patient_ID IN (
 	SELECT patID
- FROM benchling
+ FROM upGenes
 );
 
 # OUTPUT : Pat027, Pat007
@@ -95,7 +95,7 @@ WHERE Patient_ID IN (
 		GROUP BY Patient_ID) as groupedCopies
 	WHERE Patient_ID IN (
 		SELECT patID
-		FROM benchling
+		FROM upGenes
 	)
 ) AND symbol IN (
 	SELECT upHsgene
@@ -103,5 +103,36 @@ WHERE Patient_ID IN (
 )) AS wholetable, upGenes
 WHERE wholetable.Patient_ID=upGenes.patID AND wholetable.symbol=upGenes.upHsgene;
 
+# For upGenes
 # OUTPUT : Pat027 - GNAS, AURKA, TSHZ2, TSHZ3, CCNE1
 #          Pat007 - SMC1A
+
+SELECT wholetable.Patient_ID, wholetable.symbol
+FROM(
+SELECT Patient_ID, symbol
+FROM (
+	SELECT Patient_ID,symbol
+	FROM copies
+	WHERE pipeline_name LIKE '%sequenza_vivan%'
+) AS pipeline
+WHERE Patient_ID IN (
+	# patients that are in benchling&copies
+	SELECT Patient_ID
+	FROM (
+		SELECT Patient_ID
+		FROM copies
+        WHERE copy_number > 2
+		GROUP BY Patient_ID) as groupedCopies
+	WHERE Patient_ID IN (
+		SELECT patID
+		FROM benchling
+	)
+) AND symbol IN (
+	SELECT downHsgene
+    FROM downGenes
+)) AS wholetable, downGenes
+WHERE wholetable.Patient_ID=downGenes.patID AND wholetable.symbol=downGenes.downHsgene;
+
+# For downGenes
+# OUTPUT : Pat027 - TP53, KMT2C
+#          Pat007 - KDM6A, CEBPA, SMARCA4, MYO1C, KMT2D, PTEN, STAG1
